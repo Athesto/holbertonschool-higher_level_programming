f3638c8af13ecec4e4eeac8969b27a5472d39801
Athesto <gamez.live@gmail.com>
2020-09-01 21:58:35 -0500

    add end line and

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(99+1):
-    print("{}".format(i), end="")
     if i < 99:
-        print("", end=", ")
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:02d}".format(i))
