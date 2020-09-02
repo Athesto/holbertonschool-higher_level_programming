1d36da17eae099b9ae2d04a618a93d9eae650b16
Athesto <gamez.live@gmail.com>
2020-09-01 21:44:02 -0500

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(98+1):
-    print("{} = 0x{:x}".format(i, i))
+for i in range(99+1):
+    print("{}".format(i), end="")
+    if i < 99:
+        print("", end=", ")
