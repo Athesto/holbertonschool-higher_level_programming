f13cd8646cf3ae49434f4c7e833898f73e6e9ff7
Athesto <gamez.live@gmail.com>
2020-09-03 23:54:10 -0500

    remove OPERATORS and set functions as matrix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,12 +4,11 @@ if __name__ == "__main__":
     from sys import argv
 
     argc = len(argv)
-    OPERATORS = "+-*/"
-    functions = [add, sub, mul, div]
+    functions = ["+-*/", [add, sub, mul, div]]
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])
-    f_idx = OPERATORS.find(operator)
+    f_idx = functions[0].find(operator)
 
     if argc != 4:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
@@ -18,4 +17,4 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     print(*argv[1:], end=" = ")
-    print("{:d}".format(functions[f_idx](a, b)))
+    print("{:d}".format(functions[1][f_idx](a, b)))
