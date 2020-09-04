900588f104a87bf2473ff3c79fe78683045f0a2c
Athesto <gamez.live@gmail.com>
2020-09-03 23:58:41 -0500

    check before and print int values

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,17 +4,18 @@ if __name__ == "__main__":
     from sys import argv
 
     argc = len(argv)
-    functions = ["+-*/", [add, sub, mul, div]]
-    a = int(argv[1])
-    operator = argv[2]
-    b = int(argv[3])
-    f_idx = functions[0].find(operator)
-
     if argc != 4:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
+
+    functions = ["+-*/", [add, sub, mul, div]]
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+    f_idx = functions[0].find(operator)
+
     if len(operator) != 1 or f_idx == -1:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    print(*argv[1:], end=" = ")
+    print("{} {} {}".format(a, operator, b), end=" = ")
     print("{:d}".format(functions[1][f_idx](a, b)))
