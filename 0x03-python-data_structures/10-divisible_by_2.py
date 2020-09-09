3ab90a61526033bb1e236112ea7fddc58bd467f4
Athesto <gamez.live@gmail.com>
2020-09-08 23:58:20 -0500

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    return True
+    list = []
+    for i in my_list:
+        if i % 2 == 0:
+            list.append(True)
+        else:
+            list.append(False)
+    return list
