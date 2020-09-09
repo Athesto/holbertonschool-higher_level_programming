b7ee866301c98bd8754e5b14704adb140a722aa7
Athesto <gamez.live@gmail.com>
2020-09-08 23:38:40 -0500

    check with len

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not any(matrix):
+    if len(matrix[0]) is 0:
         print()
         return
     for m_idx in range(len(matrix)):
