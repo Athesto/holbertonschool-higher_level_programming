1d8a5924ee4869ea6f22bb8c2b5f7b3e5d55561b
Athesto <gamez.live@gmail.com>
2020-09-09 14:21:36 -0500

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define true 1
+#define false !true
+
+/**
+ * is_palindrome - check if it's a palindrome list
+ * @head: head list
+ * Return: 1 is true or 0 is false
+ */
+int is_palindrome(listint_t **head)
+{
+	unsigned int l_len;		/* list len */
+	listint_t *runner = *head;	/* runner pointer */
+	int *array;				/* array of numbers */
+	unsigned int a_idx;		/* array index */
+
+	/* check if it's a valid list */
+	if (!head || !(*head)->next)
+		return (true);
+
+	for (l_len = 0; runner; l_len++)
+		runner = runner->next;
+
+	printf("len = %d\n", l_len);
+	array = malloc(l_len * sizeof(*array));
+
+	/* fill the array */
+	runner = *head;
+	for (a_idx = 0; a_idx < l_len; a_idx++)
+	{
+		array[a_idx] = runner->n;
+		runner = runner->next;
+	}
+
+	/* check array */
+	for (a_idx = 0; a_idx < l_len / 2; a_idx++)
+	{
+		if ((array[a_idx] - array[l_len - a_idx - 1]) != 0)
+			return (false);
+	}
+	return (true);
+}
