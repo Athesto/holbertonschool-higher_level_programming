0f04fc7b55f46fdcecc0ff5cb6d34829235ac3df
Athesto <gamez.live@gmail.com>
2020-09-08 22:45:01 -0500

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return
+    if idx < 0 or len(my_list) - 1 < idx:
+        return my_list
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
