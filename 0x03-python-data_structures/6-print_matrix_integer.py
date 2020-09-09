abca63114e243efcf74592eb933d27c6034f5e53
Athesto <gamez.live@gmail.com>
2020-09-08 23:53:46 -0500

    linked for

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if len(matrix[0]) is 0:
         print()
         return
-    for m_idx in range(len(matrix)):
-        print("{:d} {:d} {:d}".format(*matrix[m_idx]))
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if col == len(matrix[0]) - 1:
+                print("{:d}".format(matrix[row][col]), end="")
+            else:
+                print("{:d}".format(matrix[row][col]), end=" ")
+        print()
