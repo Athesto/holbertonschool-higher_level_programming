5cc2da3d96fb2abf2d0f75ba6ae9be40a9064329
Athesto <gamez.live@gmail.com>
2020-09-09 20:14:58 -0500

    huge buf size

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #define true 1
 #define false !true
-#define BUFSIZE 1024
+#define BUFSIZE 102400
 
 /**
  * is_palindrome - check if it's a palindrome list
@@ -14,14 +14,12 @@ int is_palindrome(listint_t **head)
 {
 	unsigned int l_len;		/* list len */
 	listint_t *runner = *head;	/* runner pointer */
-	int *array;				/* array of numbers */
+	int array[BUFSIZE];				/* array of numbers */
 	unsigned int a_idx;		/* array index */
-	size_t buf_size = BUFSIZE;
 
 	/* check if it's a valid list */
 	if (!head || !*head || !(*head)->next)
 		return (true);
-	array = malloc(BUFSIZE);
 
 	/* fill the array */
 	runner = *head;
@@ -30,11 +28,6 @@ int is_palindrome(listint_t **head)
 		l_len = a_idx;
 		array[a_idx] = runner->n;
 		runner = runner->next;
-		if (a_idx > buf_size)
-		{
-			buf_size += BUFSIZE;
-			array = realloc(array, buf_size);
-		}
 	}
 	l_len++;
 
@@ -43,10 +36,8 @@ int is_palindrome(listint_t **head)
 	{
 		if ((array[a_idx] - array[l_len - a_idx - 1]) != 0)
 		{
-			free(array);
 			return (false);
 		}
 	}
-	free(array);
 	return (true);
 }
