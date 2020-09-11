ad23af45e77d0ea3655aa0112d32981509aed520
Athesto <gamez.live@gmail.com>
2020-09-10 22:04:10 -0500

    check set1 in set2

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,4 +4,7 @@ def only_diff_elements(set_1, set_2):
     for item in set_1:
         if item not in set_2:
             out.append(item)
+    for item in set_2:
+        if item not in set_1:
+            out.append(item)
     return out
