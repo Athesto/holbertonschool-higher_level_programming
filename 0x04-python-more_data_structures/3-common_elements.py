de463e188e75b26f2898145a152f672fde69bdb7
Athesto <gamez.live@gmail.com>
2020-09-11 14:55:47 -0500

    with & operator

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    out = []
-    for item in set_1:
-        if item in set_2:
-            out.append(item)
-    return out
+    return set_1 & set_2
