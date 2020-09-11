16d8b365df87f496b5281526230789ebeafc7748
Athesto <gamez.live@gmail.com>
2020-09-10 23:48:39 -0500

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sum = 0
+    if type(roman_string) is str:
+        decode = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                'C': 100, 'D': 500, 'M': 100}
+        carry = ['I', 0]
+        for c in roman_string:
+            if carry[0] == c:
+                carry[1] += decode[c]
+            elif decode[carry[0]] < decode[c]:
+                sum += decode[c] - carry[1]
+                carry[0] = c
+                carry[1] = 0
+            else:
+                sum += decode[c] + carry[1]
+                carry[0] = c
+                carry[1] = 0
+        sum += carry[1]
+    return sum
+
