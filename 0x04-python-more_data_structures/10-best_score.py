bc7889255d72e44e581d1ca9fe95c8943a78c037
Athesto <gamez.live@gmail.com>
2020-09-10 22:54:05 -0500

    pep8 review

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
-    k,v = sorted(a_dictionary.items(), key = lambda x: x[1], reverse=True)[0]
+    k, v = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)[0]
     return (k)
