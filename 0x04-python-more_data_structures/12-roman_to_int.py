6aae9e9e2bca6e29f649aedd334e03b6de0d6823
Athesto <gamez.live@gmail.com>
2020-09-11 00:51:26 -0500

    init with string val, decode to carry

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     sum = 0
-    if type(roman_string) is str:
+    if type(roman_string) is str and roman_string is not "":
         decode = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
-        carry = ['I', 0]
+        carry = [roman_string[0], 0]
         for c in roman_string:
             if c not in decode:
                 return 0
             if carry[0] == c:
                 carry[1] += decode[c]
             elif decode[carry[0]] < decode[c]:
-                sum += decode[c] - carry[1]
+                sum +=  - carry[1]
                 carry[0] = c
-                carry[1] = 0
+                carry[1] = decode[c]
             else:
-                sum += decode[c] + carry[1]
+                sum += carry[1]
                 carry[0] = c
-                carry[1] = 0
+                carry[1] = decode[c]
         sum += carry[1]
     return sum
