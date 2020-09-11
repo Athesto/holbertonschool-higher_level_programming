baadc29ccf9ebbee61beb94fe957bceaf2070d3f
Athesto <gamez.live@gmail.com>
2020-09-11 13:51:36 -0500

    using set operators

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    out = []
-    for item in set_1:
-        if item not in set_2:
-            out.append(item)
-    for item in set_2:
-        if item not in set_1:
-            out.append(item)
-    return out
+   return set_1 ^ set_2
