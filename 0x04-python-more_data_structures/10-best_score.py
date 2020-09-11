f7d6f25508dcda1b23182f34383a725305128501
Athesto <gamez.live@gmail.com>
2020-09-10 22:58:43 -0500

    if valid

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    k, v = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)[0]
-    return (k)
+    if a_dictionary:
+        k, v = sorted(a_dictionary.items(),
+                      key=lambda x: x[1], reverse=True)[0]
+        return (k)
