0ae879f5b9c027becabbdf5c870e991a55117cd7
Athesto <gamez.live@gmail.com>
2020-09-21 22:30:58 -0500

    more case

--- 0x05-python-exceptions/test/2-main.py
+++ 0x05-python-exceptions/test/2-main.py
@@ -13,3 +13,9 @@ print("nb_print: {:d}".format(nb_print))
 
 nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers([[], 10], len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers([10, "hello", 10, 10, [], 10], 20)
+print("nb_print: {:d}".format(nb_print))
