403c84275bc6c4b01485e827143d0e990e7c9f94
Athesto <gamez.live@gmail.com>
2020-09-21 22:51:03 -0500

    check betty

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -20,7 +20,6 @@ void print_python_list(PyObject *p)
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %d\n", allocated);
-		
 
 	(void)p;
 }
