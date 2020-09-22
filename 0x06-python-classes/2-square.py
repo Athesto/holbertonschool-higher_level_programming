a09b857ae9b40c197b7f434673ee3191e2993971
Athesto <gamez.live@gmail.com>
2020-09-22 17:42:35 -0500

    addend size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,10 +6,10 @@ class Square:
     '''Square with size review'''
     def __init__(self, size=0):
         try:
-            self.__size = size
             if size < 0:
                 raise ValueError("size must be >= 0")
             if type(size) != int:
                 raise TypeError
+            self.__size = size
         except TypeError:
             raise NameError("size must be an integer")
