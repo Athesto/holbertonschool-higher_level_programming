45a852b4238ab142bc1818a7cf1c41bd6f005934
Athesto <gamez.live@gmail.com>
2020-09-22 19:21:59 -0500

    remove try and use if

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,17 +16,12 @@ class Square:
     @size.setter
     def size(self, value):
         '''setSize'''
-        try:
-            if type(value) is not int:
-                raise TypeError
-            elif value < 0:
-                raise ValueError
-            else:
-                self.__size = value
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         '''check area'''
