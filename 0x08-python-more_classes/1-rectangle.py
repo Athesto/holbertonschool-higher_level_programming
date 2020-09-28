141562a1d1f62d737b807f951f1928b67f4b3955
Athesto <gamez.live@gmail.com>
2020-09-28 23:57:01 +0000

    change syntax for __dict__ output

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     '''Rectangle Class'''
     def __init__(self, width=0, height=0):
         '''constuctor'''
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
