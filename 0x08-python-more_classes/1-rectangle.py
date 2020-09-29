3b59cb3442dc7625eb35d079c01ab043773d7e76
Athesto <gamez.live@gmail.com>
2020-09-29 00:23:14 +0000

    return type and fix self.height getter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         '''setter'''
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -27,14 +27,14 @@ class Rectangle:
     @property
     def height(self):
         '''getter'''
-        return self.__heigth
+        return self.__height
 
     @height.setter
     def height(self, value):
         '''setter'''
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("height must be >= 0")
+            raise TypeError("height must be >= 0")
         else:
             self.__height = value
