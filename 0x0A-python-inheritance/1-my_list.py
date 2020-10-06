1e43b34e8f1c41d37970379ab9d06c2c6a9f7353
Athesto <gamez.live@gmail.com>
2020-10-06 18:52:52 -0500

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/python3
+'''Task 1'''
+
+
+class MyList(list):
+    '''My list Class'''
+    def print_sorted(self):
+        '''print_sorted'''
+        list_sorted = self[:]
+        list_sorted.sort()
+        print(list_sorted)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
