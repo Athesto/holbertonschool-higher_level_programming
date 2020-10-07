85fff8e6cc8edcbfa80a617f4c8881409fd33ce1
Athesto <gamez.live@gmail.com>
2020-10-07 17:57:48 -0500

    documentation function

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-'''task 0'''
+'''task 1'''
 
 
 def number_of_lines(filename=""):
+    '''number of lines'''
     line_counter = 0
     with open(filename, encoding="utf-8") as f:
         for line in f:
             line_counter += 1
-
     return line_counter
