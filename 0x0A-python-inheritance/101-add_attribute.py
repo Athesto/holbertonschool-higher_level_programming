6ca49141607db1ee1b83542eafb11ccd021f2a07
Athesto <gamez.live@gmail.com>
2020-10-07 00:11:49 -0500

    fix pep8

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,7 @@
 def add_attribute(obj, attribute, value):
     '''add attribute'''
 
-    #print(dir(obj))
+    # print(dir(obj))
     if '__add__' in dir(obj):
         raise TypeError("can't add new attribute")
     setattr(obj, attribute, value)
