6e041c2e9c0a24eb70ce90fa40ff950c258e9bf7
Athesto <gamez.live@gmail.com>
2020-10-07 17:20:59 -0500

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''task 0'''
+
+
+def read_file(filename=""):
+    '''read file'''
+    with open(filename, 'r', encoding="utf-8") as file:
+        for line in file:
+            print(line)
