858bfab8b8670cf881ea6a4ae481fd0cf02e04dd
Athesto <gamez.live@gmail.com>
2020-10-06 19:04:21 -0500

    documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''module task2'''
+
+
 def is_same_class(obj, a_class):
+    '''function is_same_class'''
     return type(obj) is a_class
     # return type(obj).__name__ ==  a_class.__name__
