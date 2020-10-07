5470a16fee0bac5247d426fc9d5c73b927006bb5
Athesto <gamez.live@gmail.com>
2020-10-07 00:11:04 -0500

    101-add_attribute.py

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''advanced 101'''
+
+
+def add_attribute(obj, attribute, value):
+    '''add attribute'''
+
+    #print(dir(obj))
+    if '__add__' in dir(obj):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attribute, value)
