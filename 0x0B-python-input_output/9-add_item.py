73bb7992f8efed45ed95102b1a19cf52b1bbcf6b
Athesto <gamez.live@gmail.com>
2020-10-07 20:00:00 -0500

    9-add_item.py

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+filename = 'add_item.json'
+
+try:
+    data = load_from_json_file(filename)
+except:
+    data = []
+data.extend(sys.argv[1:])
+save_to_json_file(data, filename)
