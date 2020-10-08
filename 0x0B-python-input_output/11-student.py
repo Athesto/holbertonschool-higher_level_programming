90a417cc37982ec2b26b794fbbafe44d2004bd93
Athesto <gamez.live@gmail.com>
2020-10-07 22:33:28 -0500

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''task 11'''
+
+
+class Student():
+    '''student'''
+
+    def __init__(self, first_name, last_name, age):
+        '''constructor'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''to_json'''
+        return self.__dict__
