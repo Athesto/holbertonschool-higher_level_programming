92cbc43ef506685a37d102e67e1d1a5e86bcb280
Athesto <gamez.live@gmail.com>
2020-10-13 13:52:59 -0500

    test ok
    test str width
    
    str height

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''module test rectangle'''
+
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    '''Testing Rectangle'''
+
+    def test_str_width(self):
+        '''TypeError: str_width'''
+        with self.assertRaises(TypeError) as ar:
+            Rectangle("11", 10)
+        self.assertEqual(str(ar.exception), "width must be an integer")
+
+    def test_str_height(self):
+        '''TypeError: str_height'''
+        with self.assertRaises(TypeError) as ar:
+            Rectangle(10, "11")
+        self.assertEqual(str(ar.exception), "height must be an integer")
+
+
+if __name__ == "__main__":
+    __import__("sys").path.append('.')
+    unittest.main()
