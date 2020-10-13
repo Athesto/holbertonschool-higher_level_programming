1c785eb9f3023dd056061affe6a42e9d8fc61c04
Athesto <gamez.live@gmail.com>
2020-10-13 16:14:03 -0500

    test x and y

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,17 +14,31 @@ class TestRectangle(unittest.TestCase):
             Rectangle("11", 10)
         self.assertEqual(str(ar.exception), "width must be an integer")
 
-    def test_bol_width(self):
-        '''TypeError: bol_width'''
-        with self.assertRaises(TypeError) as ar:
-            Rectangle(True, 10)
-        self.assertEqual(str(ar.exception), "width must be an integer")
-
     def test_str_height(self):
         '''TypeError: str_height'''
         with self.assertRaises(TypeError) as ar:
             Rectangle(10, "11")
         self.assertEqual(str(ar.exception), "height must be an integer")
+
+    def test_str_x(self):
+        '''TypeError: str_x'''
+        with self.assertRaises(TypeError) as ar:
+            Rectangle(10, 11, "hello")
+        self.assertEqual(str(ar.exception), "x must be an integer")
+
+    def test_str_y(self):
+        '''TypeError: str_y'''
+        with self.assertRaises(TypeError) as ar:
+            Rectangle(10, 11, 10, "hello")
+        self.assertEqual(str(ar.exception), "y must be an integer")
+
+    # ------------------UNUSEFUL--------------------
+
+    def test_bol_width(self):
+        '''TypeError: bol_width'''
+        with self.assertRaises(TypeError) as ar:
+            Rectangle(True, 10)
+        self.assertEqual(str(ar.exception), "width must be an integer")
 
 
 if __name__ == "__main__":
