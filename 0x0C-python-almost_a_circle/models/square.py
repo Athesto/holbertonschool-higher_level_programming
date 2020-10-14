bc3765b1806e207fe535903971c67aa573ab3f19
Athesto <gamez.live@gmail.com>
2020-10-14 00:11:26 -0500

    getter/setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,7 +18,22 @@ class Square(Rectangle):
             raise ValueError("size must be > 0")
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if not value > 0:
+            raise ValueError("size must be > 0")
+        self.width = value
+        self.height = value
+
     def __str__(self):
+        '''square information'''
         return '[Square] ({}) {}/{} - {}'.format(self.id,
                                                  self.x,
                                                  self.y,
