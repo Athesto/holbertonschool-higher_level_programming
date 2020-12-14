5adb1792d800438aab73145bb8548bc92b3fce82
Athesto <gamez.live@gmail.com>
2020-12-14 03:36:21 +0000

    0 show data

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,27 @@
 #!/usr/bin/python3
 """task from holberton"""
-pass
+
+import MySQLdb
+import sys
+
+
+data = {
+    "host": "localhost",
+    "port": "3306",
+    "user": sys.argv[1],
+    "passwd": sys.argv[2],
+    "db": sys.argv[3]
+}
+query = "SELECT * from states"
+
+db = MySQLdb.connect(
+    host=data["host"],
+    user=data["user"],
+    passwd=data["passwd"],
+    db=data["db"])
+
+cur = db.cursor()
+cur.execute(query)
+row = cur.fetchall()
+
+print(*row, sep="\n")
