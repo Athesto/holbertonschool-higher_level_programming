10bea468602b674dd13ce71aaaf6aff53456018c
Athesto <gamez.live@gmail.com>
2020-12-14 03:45:39 +0000

    exec as module

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """task from holberton"""
 
-import MySQLdb
-import sys
+if __name__ is "__main__":
+    import MySQLdb
+    import sys
 
+    data = {
+        "host": "localhost",
+        "port": "3306",
+        "user": sys.argv[1],
+        "passwd": sys.argv[2],
+        "db": sys.argv[3]
+    }
+    query = "SELECT * from states ORDER BY id ASC"
 
-data = {
-    "host": "localhost",
-    "port": "3306",
-    "user": sys.argv[1],
-    "passwd": sys.argv[2],
-    "db": sys.argv[3]
-}
-query = "SELECT * from states ORDER BY id ASC"
+    db = MySQLdb.connect(
+        host=data["host"],
+        user=data["user"],
+        passwd=data["passwd"],
+        db=data["db"])
 
-db = MySQLdb.connect(
-    host=data["host"],
-    user=data["user"],
-    passwd=data["passwd"],
-    db=data["db"])
+    cur = db.cursor()
+    cur.execute(query)
+    row = cur.fetchall()
 
-cur = db.cursor()
-cur.execute(query)
-row = cur.fetchall()
-
-print(*row, sep="\n")
+    print(*row, sep="\n")
