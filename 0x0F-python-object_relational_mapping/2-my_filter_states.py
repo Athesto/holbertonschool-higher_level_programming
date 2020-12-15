db0d434beb7477093753315024894821f28e7c0c
Athesto <gamez.live@gmail.com>
2020-12-15 14:16:18 -0500

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
 """task from holberton"""
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    credentials = {
+        "host": "localhost",
+        "port": 3306,
+        "user": argv[1],
+        "passwd": argv[2],
+        "db": argv[3]
+    }
+
+    con = MySQLdb.connect(**credentials)
+    query = "SELECT * FROM states WHERE name = '{}'".format(argv[4])
+
+    with con.cursor() as cur:
+        cur.execute(query)
+        results = cur.fetchall()
+
+        for line in results:
+            print(line)
+
+    con.close()
