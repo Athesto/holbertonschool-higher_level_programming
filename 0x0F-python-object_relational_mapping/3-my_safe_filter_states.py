73302e36fd62432950c89aa509e34ded691d05c0
Athesto <gamez.live@gmail.com>
2020-12-15 14:35:55 -0500

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
 """task from holberton"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    credentials = {
+        "host": "localhost",
+        "port": 3306,
+        "user": sys.argv[1],
+        "passwd": sys.argv[2],
+        "db": sys.argv[3]
+    }
+    word = sys.argv[4]
+
+    if word.isalpha():
+        query = "select * from states where binary name = '{}'".format(word)
+        con = MySQLdb.connect(**credentials)
+        with con.cursor() as cursor:
+            cursor.execute(query)
+            results = cursor.fetchall()
+            for line in results:
+                print(line)
+        con.close()
