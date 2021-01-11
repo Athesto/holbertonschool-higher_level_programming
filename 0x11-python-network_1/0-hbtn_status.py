b607e41c400813a4c65f839489e492dfc0c86d31
Athesto <gamez.live@gmail.com>
2021-01-11 14:50:16 -0500

    set first message

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         html = response.read()
+        print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- uft8 content: {}'.format(html.decode('utf-8')))
