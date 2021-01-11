67d56510f7b7e41d9ff94ef9beb8d7fb7b82aebe
Athesto <gamez.live@gmail.com>
2021-01-11 18:46:12 -0500

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''
+script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response
+'''
+
+if __name__ == '__main__':
+    from urllib import request
+    from sys import argv
+    url = argv[1]
+    email = argv[2]
+
+    with request.urlopen(url) as req:
+        print("Your email is: {}".format(email))
