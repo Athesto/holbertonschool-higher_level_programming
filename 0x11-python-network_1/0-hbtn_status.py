6d616a128321189ac033baf72fd521a65c064f7d
Athesto <gamez.live@gmail.com>
2021-01-11 14:42:07 -0500

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+'''a Python script that fetches https://intranet.hbtn.io/status'''
+
+if __name__ == "__main__":
+    import urllib.request
+
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- uft8 content: {}'.format(html.decode('utf-8')))
