df401d4815ff36cd23967dac4113d6828e07e4f5
Athesto <gamez.live@gmail.com>
2021-01-12 02:45:52 -0500

    use dict, json with array and for item in array

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,11 +7,13 @@ rails by the user rails
 if __name__ == "__main__":
     from sys import argv
     from requests import get
-    repo = argv[1]
-    owner = argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    args = {
+        "repo": argv[1],
+        "owner": argv[2]
+    }
+    url = "https://api.github.com/repos/{owner}/{repo}/commits".format(**args)
 
-    req = get(url)
+    req = get(url).json()[:10]
 
-    for i in range(10):
-        print("{sha}: {author[login]}".format(**req.json()[i]))
+    for item in req:
+        print("{sha}: {commit[author][name]}".format(**item))
