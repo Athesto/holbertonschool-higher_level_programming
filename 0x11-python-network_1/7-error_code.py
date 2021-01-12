719e3512b3ee22fdd7c919da3ef1c48a746d1baf
Athesto <gamez.live@gmail.com>
2021-01-12 04:09:39 +0000

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''
+A script that takes in a URL, sends a request to the URL
+and displays the body of the response.
+'''
+
+
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+    url = argv[1]
+
+    req = get(url)
+    if (req.status_code) < 400:
+        print(req.text)
+    else:
+        print("Error code: {}".format(req.status_code))
