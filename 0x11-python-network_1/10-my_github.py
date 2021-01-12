44965f2045d6e05e0875270b42f17f1dbbbffe06
Athesto <gamez.live@gmail.com>
2021-01-12 05:00:04 +0000

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+'''
+a Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
+'''
+
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+    myobj = {
+        "url":  "https://api.github.com/user",
+        "auth":
+            (
+                argv[1],
+                argv[2]
+            )
+    }
+    req = get(**myobj)
+    try:
+        print(req.json().get('id'))
+    except:
+        print("Not valid JSON")
