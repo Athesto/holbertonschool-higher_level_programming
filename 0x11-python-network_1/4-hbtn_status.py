d797ff7ce5f150c7d4a35ad1e7f278bd1926e48a
Athesto <gamez.live@gmail.com>
2021-01-12 03:33:59 +0000

    using module requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,10 +2,11 @@
 '''script that fetches https://intranet.hbtn.io/status'''
 
 if __name__ == '__main__':
-    from urllib import request
+    from requests import get
     url = "https://intranet.hbtn.io/status"
-    with request.urlopen(url) as req:
-        ans = req.read().decode('utf-8')
-        print("Body response:")
-        print("\t- type: {}".format(type(ans)))
-        print("\t- content: {}".format(ans))
+    req = get(url)
+    dir(req)
+    ans = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(ans)))
+    print("\t- content: {}".format(ans))
