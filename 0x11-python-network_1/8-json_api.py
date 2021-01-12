c9bedbd3cf6f10c9e2acb2a1ac7ae984932325d6
Athesto <gamez.live@gmail.com>
2021-01-12 04:33:34 +0000

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''
+script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+'''
+
+if __name__ == '__main__':
+    from sys import argv
+    from requests import post
+    myobj = {
+        "url": "http://0.0.0.0:5000/search_user",
+        "data": {
+            "q": "" if len(argv) < 2 else argv[1]
+        }
+    }
+    req = post(**myobj)
+    try:
+        print("[{id}] {name}".format(**req.json()))
+    except:
+        print("No result")
