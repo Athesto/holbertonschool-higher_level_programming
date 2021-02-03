464711f7939956a4fcfc6978847a3136d12bb858
Athesto <gamez.live@gmail.com>
2021-02-03 02:32:48 +0000

    include Missing occurrences

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const times = process.argv[2];
-const msg = 'C is fun\n'.repeat(times).slice(0, -1);
+let msg;
+if (parseInt(times)) { msg = 'C is fun\n'.repeat(times).slice(0, -1); } else { msg = 'Missing number of occurrences'; }
 console.log(msg);
