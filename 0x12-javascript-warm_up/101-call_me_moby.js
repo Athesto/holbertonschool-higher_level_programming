46a3fd89fb0f03729b33f821d04b7d9daa67edad
Athesto <gamez.live@gmail.com>
2021-02-03 04:41:10 +0000

    fix style

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 
-exports.callMeMoby = (x, theFunction) => {for (let i = 0; i < x; i++) theFunction()};
+exports.callMeMoby = (x, theFunction) => { for (let i = 0; i < x; i++) theFunction(); };
