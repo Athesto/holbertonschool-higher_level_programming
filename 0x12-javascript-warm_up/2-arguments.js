ee9f694ab0f97f1365713f6a253624af7e87e9fd
Athesto <gamez.live@gmail.com>
2021-02-03 01:18:02 +0000

    set args Argument and Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,5 +2,9 @@
 
 const input = process.argv;
 let msg;
-if (input.length < 3) { msg = 'No argument'; } else { msg = 'Argument found'; }
+if (input.length < 3) {
+  msg = 'No argument';
+} else {
+  msg = (input.length === 3) ? 'Argument found' : 'Arguments found';
+}
 console.log(msg);
