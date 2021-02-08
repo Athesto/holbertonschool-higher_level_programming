e4f6efb1a899ec494a2fe5eb0ba78378630d074a
Athesto <gamez.live@gmail.com>
2021-02-08 04:16:54 +0000

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print("DONE")
+    out = ""
+    for c in str:
+        if ('a' <= c <= 'z') or ('A' <= c <= 'Z'):
+            out += "{:c}".format(ord(c) & ord('_'))
+        else:
+            out += c
+    print(out)
