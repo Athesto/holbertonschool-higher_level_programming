690712f9459eb0c5e8bb8b92e7f6b95b3c03e4ff
Athesto <gamez.live@gmail.com>
2021-02-09 23:07:10 +0000

    print error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const file = process.argv.slice(2, process.argv.length);
-const print = (error, msg) => { if (!error) console.log(msg); };
-fs.readFile(file[0], 'utf-8', (error, data) => print(error, data.toString()));
+fs.readFile(file[0], 'utf-8', function (err, data) {
+  if (err) { return console.log(err); }
+  console.log(data);
+});
