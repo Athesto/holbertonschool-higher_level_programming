a590a8db8669d8651185b92ffd5c03801dcbb688
Athesto <gamez.live@gmail.com>
2021-02-09 23:02:00 +0000

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv.slice(2, process.argv.length);
+const print = (error, msg) => { if (!error) console.log(msg); };
+fs.readFile(file[0], (error, data) => print(error, data.toString()));
