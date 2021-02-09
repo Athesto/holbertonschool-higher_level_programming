09b18a7cf16a5a99c411cf5909d8ecb091fe471b
Athesto <gamez.live@gmail.com>
2021-02-09 02:14:51 +0000

    index as parameter

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 const array = require('./100-data.js').list;
+const out = array.map((x, index) => x * index);
 console.log(array);
-if (array) {
-  const out = array.map(x => x * array.indexOf(x));
-  console.log(out);
-}
+console.log(out);
