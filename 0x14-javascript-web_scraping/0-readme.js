d210dee9e3d863ef9e8af8b3026f81b580ff5daf
Athesto <gamez.live@gmail.com>
2021-02-09 23:04:44 +0000

    encode utf-8

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,4 +2,4 @@
 const fs = require('fs');
 const file = process.argv.slice(2, process.argv.length);
 const print = (error, msg) => { if (!error) console.log(msg); };
-fs.readFile(file[0], (error, data) => print(error, data.toString()));
+fs.readFile(file[0], 'utf-8', (error, data) => print(error, data.toString()));
