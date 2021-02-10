5d1c9a0e33f5d92cc81172558bf27226386cf9a0
Athesto <gamez.live@gmail.com>
2021-02-10 00:54:22 +0000

    apiURL as parameter1

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 const characterId = 18;
-request.get('https://swapi-api.hbtn.io/api/films/', function (err, resp, body) {
+const apiUrl = process.argv[2];
+request.get(apiUrl, function (err, resp, body) {
   if (err) console.error(err);
   const info = JSON.parse(body);
   const movies = info.results;
