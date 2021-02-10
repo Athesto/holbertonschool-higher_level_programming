b7d290b42e39b14ea7b50173a60726543d28a4df
Athesto <gamez.live@gmail.com>
2021-02-10 05:44:30 +0000

    include sub-request

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrlFilm = 'https://swapi-api.hbtn.io/api/films/';
-const apiUrlPeople = 'https://swapi-api.hbtn.io/api/people/';
 const film = process.argv[2];
 request
-  .get(apiUrlPeople, function (err, response, body) {
+  .get(apiUrlFilm + film + '/', function (err, response, body) {
     if (err) return console.error(err);
-    const info = JSON.parse(body).results;
+    const info = JSON.parse(body).characters;
     for (const index in info) {
-      if (info[index].films.includes(apiUrlFilm + film + '/')) {
-        console.log(info[index].name);
-      }
+      request
+        .get(info[index], function (err, resp, body) {
+          if (err) return console.error(err);
+          console.log(JSON.parse(body).name);
+        });
     }
   });
