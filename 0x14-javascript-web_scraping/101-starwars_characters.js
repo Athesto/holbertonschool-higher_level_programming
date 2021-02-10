272a5c607f680cba1016a5d9db939d1f038d884e
Athesto <gamez.live@gmail.com>
2021-02-10 16:22:23 +0000

    Timeout

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,6 +2,8 @@
 const request = require('request');
 const apiUrlFilm = 'https://swapi-api.hbtn.io/api/films/';
 const film = process.argv[2];
+
+const ans = {};
 request
   .get(apiUrlFilm + film + '/', function (err, response, body) {
     if (err) return console.error(err);
@@ -10,7 +12,14 @@ request
       request
         .get(info[index], function (err, resp, body) {
           if (err) return console.error(err);
-          console.log(JSON.parse(body).name);
+          ans[index] = JSON.parse(body).name;
         });
     }
   });
+setTimeout(() => print(ans), 2000);
+
+function print (dict) {
+  for (const idx in dict) {
+    console.log(dict[idx]);
+  }
+}
