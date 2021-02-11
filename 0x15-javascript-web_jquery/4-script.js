e1e0b8a228fd7687845dc9b99052125b8db2e2ab
Athesto <gamez.live@gmail.com>
2021-02-11 21:09:41 +0000

    toggleClass

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,10 +1,3 @@
 $('div#toggle_header').click(function () {
-  const header = $('header');
-  if (header.hasClass('red')) {
-    header.removeClass('red');
-    header.addClass('green');
-  } else if (header.hasClass('green')) {
-    header.removeClass('green');
-    header.addClass('red');
-  }
+  $('header').toggleClass('red green');
 });
