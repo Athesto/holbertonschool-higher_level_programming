5de9c7fa35247ae27c488f1a4ed1db8f7aa6bd5e
Athesto <gamez.live@gmail.com>
2021-05-25 16:09:54 -0500

    a better syntax

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 /**
- * compare_nodes - run thro
+ * recursive_cmp - run thro
  * @forward: a forward runner of the list
  * @backward: the same runner bit this will run backward
  * Return: 0, is not palindrome or 1 if is palindrome
  */
-static int compare_nodes(listint_t **forward, listint_t *backward)
+static int recursive_cmp(listint_t **forward, listint_t *backward)
 {
-	int is_pal = 0;
+	int is_pal;
 
 	if (backward == NULL)
 		return (1);
 
-	is_pal = compare_nodes(forward, backward->next);
+	is_pal = recursive_cmp(forward, backward->next);
 	is_pal = is_pal && ((*forward)->n == backward->n);
 	*forward = (*forward)->next;
 	return (is_pal);
@@ -27,5 +27,5 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *runner = *head;
 
-	return (compare_nodes(&runner, runner));
+	return (recursive_cmp(&runner, runner));
 }
